c503ceecbe84ab7533a081c9a98221b6a1023e61
holyshared <holy.shared.design@gmail.com>
2019-02-06 18:13:02 +0900

    add /

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
 
+app.get('/', (req, res, next) => {
+  console.log('GET /');
+});
+
 app.get('/callback', (req, res, next) => {
+  console.log('GET /callback');
   console.log(req.params.code);
 });
 
