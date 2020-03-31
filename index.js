90cedea419296cee479e9e3d1bb8db2b0d9fe422
holyshared <holy.shared.design@gmail.com>
2020-03-31 18:26:02 +0900

    fix code_verifier

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const querystring = require('querystring');
 
 const app = express();
 
@@ -14,40 +12,10 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/callback', (req, res, next) => {
-  console.log('GET /callback');
-  console.log(req.body.code);
-  console.log(req.params.code);
-
-  console.log(req.body);
-  console.log(req.params);
-
-  const code = req.query.code || req.body.code || req.params.code;
-
-  const CLIENT_ID = process.env.CLIENT_ID;
-  const CLIENT_SECRET = process.env.CLIENT_SECRET;
-
-  const buff = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`);
-  const credential = buff.toString('base64');
-
-  const instance = axios.create({
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Authorization": `Basic ${credential}`,
-    }
-  });
-  const payload = querystring.stringify({
-    grant_type: 'authorization_code',
-    code
-  });
-  axios.post('https://oauth.chatwork.com/token', payload)
-  .then(function (res) {
-    console.log(res.data);
-    res.status(200).end('Ok');
-  })
-  .catch(function (error) {
-    console.log(error.message);
-    res.status(503).end('Oops!!');
-  });
+  const code = req.query.code;
+  console.log('code------------------');
+  console.log(code);
+  res.status(200).end('Ok');
 });
 
 app.get('/token', (req, res, next) => {
